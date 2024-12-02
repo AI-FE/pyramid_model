964b04fc1cb03493a7b9118a098eaca62ce58454
kasong <313439271@qq.com>
2024-12-02 12:16:40 +0800

    fix: rm history

--- .history/app/page_20241029135207.tsx
+++
@@ -1,5 +0,0 @@
-import { NonDuplicateLevelWiseSequentialFlowChart } from "@/components/non-duplicate-level-wise-sequential-flow-chart"
-
-export default function Page() {
-  return <NonDuplicateLevelWiseSequentialFlowChart />
-}
--- .history/app/page_20241029141218.tsx
+++
@@ -1,5 +0,0 @@
-import { FlowChart } from "@/components/non-duplicate-level-wise-sequential-flow-chart";
-
-export default function Page() {
-  return <FlowChart />;
-}
